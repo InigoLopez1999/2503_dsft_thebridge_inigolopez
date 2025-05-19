ba6a3e3d6004ced42373bdf2686a7362266b7ee9
InigoLopez1999 <iniloay@gmail.com>
2025-05-19 12:25:40 +0200

    s

--- 4-Data_Engineering/1-APIs/APIs_Local/api_marketing.py
+++ 4-Data_Engineering/1-APIs/APIs_Local/api_marketing.py
@@ -10,6 +10,11 @@ from flask import Flask, jsonify, request, abort
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+print(__file__)
+
+print(os.path.dirname(__file__))
+os.chdir(os.path.dirname(__file__))
+
 # End Point "/"
 @app.route('/', methods=['GET'])
 def home():
@@ -34,6 +39,27 @@ def predict():
         prediction = model.predict([[float(tv),float(radio),float(newspaper)]])
     
     return jsonify({'predictions': prediction[0]})
+
+@app.route('/v1/retrain', methods=['GET'])
+def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
+    if os.path.exists("data/Advertising_new.csv"):
+        data = pd.read_csv('data/Advertising_new.csv')
+
+        X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
+                                                        data['sales'],
+                                                        test_size = 0.20,
+                                                        random_state=42)
+
+        model = Lasso(alpha=6000)
+        model.fit(X_train, y_train)
+        rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
+        mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
+        model.fit(data.drop(columns=['sales']), data['sales'])
+        pickle.dump(model, open('ad_model.pkl', 'wb'))
+
+        return f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
+    else:
+        return f"<h2>New data for retrain NOT FOUND. Nothing done!</h2>"
 
 
 app.run()
